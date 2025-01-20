6c164bd6d065dd7ee0c2601c7f9178f2571e94af
Benedict Jackson <bavjackson@gmail.com>
2025-01-20 16:48:43 +0000

    fix typo

--- keyboards/sofle/keymaps/bavjackson/keymap.c
+++ keyboards/sofle/keymaps/bavjackson/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  _______,    KC_1,    KC_2,   KC_Up,    KC_4,    KC_5,                       KC_PLUS, KC_7,    KC_8,    KC_9, KC_PAST,  KC_F12,
+  _______,    KC_1,    KC_2,   KC_UP,    KC_4,    KC_5,                       KC_PLUS, KC_7,    KC_8,    KC_9, KC_PAST,  KC_F12,
   _______, KC_EXLM, KC_LEFT, KC_DOWN, KC_RGHT, KC_PERC,                       KC_MINS, KC_4,    KC_5,    KC_6, KC_PSLS, KC_DEL,
   _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,       _______, KC_0,  KC_1,    KC_2,    KC_3, KC_PENT, _______,
                        _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
