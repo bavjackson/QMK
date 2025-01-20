63d4e5f58c4343767d7677fc8d5879b5856e32f4
Benedict Jackson <bavjackson@gmail.com>
2025-01-20 17:09:10 +0000

    switch raise and enter

--- keyboards/sofle/keymaps/bavjackson/keymap.c
+++ keyboards/sofle/keymaps/bavjackson/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS,
   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,   KC_PSCR,  KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_LGUI,KC_LALT,KC_LCTL, KC_BSPC, TL_LOWR,      TL_UPPR,  KC_SPC, KC_ENT, KC_RALT, KC_RCTL
+                 KC_LGUI,KC_LALT,KC_LCTL, KC_BSPC, TL_LOWR,      KC_ENT,  KC_SPC,  TL_UPPR, KC_RALT, KC_RCTL
 ),
 
 /* LOWER
