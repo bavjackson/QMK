2814491a9345232a608df0ca9c2961bd9307a1c5
Benedict Jackson <bavjackson@gmail.com>
2025-03-01 12:34:15 +0000

    try adding homerow mods

--- keyboards/sofle/keymaps/bavjackson/keymap.c
+++ keyboards/sofle/keymaps/bavjackson/keymap.c
@@ -19,6 +19,19 @@ enum custom_keycodes {
 
 #define KC_QWERTY PDF(_QWERTY)
 
+#define HOME_A LGUI_T(KC_A)
+#define HOME_S LSFT_T(KC_S)
+#define HOME_D LALT_T(KC_D)
+#define HOME_F LCTL_T(KC_F)
+
+#define HOME_J RCTL_T(KC_J)
+#define HOME_K RALT_T(KC_K)
+#define HOME_L RSFT_T(KC_L)
+#define HOME_SCLN RGUI_T(KC_SCLN)
+
+#define TAPPING_TERM 200
+#define QUICK_TAP_TERM 0
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * QWERTY
@@ -39,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS,
-  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_ESC,   HOME_A, HOME_S,  HOME_D,  HOME_F,  KC_G,                      KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN,KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,   KC_PSCR,  KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                  KC_LGUI,KC_LALT,KC_LCTL, KC_BSPC, TL_LOWR,      KC_ENT,  KC_SPC,  TL_UPPR, KC_RALT, KC_RCTL
 ),
